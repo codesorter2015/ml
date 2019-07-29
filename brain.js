455d288572a5888498039aef87ccd03bce4643e3
Krish <codesorter2015@gmail.com>
2019-07-29 15:38:38 +0530

    Create brain.js

---
+++ brain.js
@@ -0,0 +1,16 @@
+https://scrimba.com/p/pVZJQfg/cv4rvCR 
+
+Propogation,forward and backward
+layers and neurons
+training and errors
+feed forward nerworks
+recurrent networks
+
+What we'll build:
+1. XOR
+2. Counter
+3. Basic math network
+4. Image Recognizer
+5. Sentiment Analyzer 
+6. Children's book creator
+
